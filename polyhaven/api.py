4f34917e2deca64d114a1db9924a6e222daf6e31
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2025-12-18 22:19:04 +0300

    Refactor API module to use constants for headers and timeout; add tests for API availability

--- polyhaven/api.py
+++ polyhaven/api.py
@@ -2,11 +2,19 @@ from urllib.request import urlopen, Request
 import json
 
 BASE_URL = "https://api.polyhaven.com"
+HEADERS = {"User-Agent": "PolyHavenNukeBrowser"}
+TIMEOUT = 10
 
 def get_types() -> list[str]:
-    req = Request(url = f"{BASE_URL}/types", headers = {"User-Agent": "PolyHavenNukeBrowser"})
-    with urlopen(req, timeout=10) as resp:
-        body: bytes = resp.read()
+    req = Request(url=f"{BASE_URL}/types", headers=HEADERS)
+    with urlopen(req, timeout=TIMEOUT) as resp:
+        body = resp.read()
         data = json.loads(body.decode("utf-8"))
 
     return data
+
+
+def is_api_available() -> bool:
+    req = Request(url=BASE_URL, headers=HEADERS)
+    with urlopen(req, timeout=TIMEOUT) as resp:
+        return resp.getcode() == 200
--- tests/test_polyhaven_api.py
+++ tests/test_polyhaven_api.py
@@ -1,4 +1,8 @@
-from polyhaven.api import get_types
+from polyhaven.api import get_types, is_api_available
 
 def test_get_types():
     assert get_types() == ["hdris", "textures", "models"]
+
+
+def test_is_api_available():
+    assert is_api_available() is True
